f9b6c37e935c99a2d0d30074b2139ca63c8ba3ee
hernan lombardo <lombardo.h@hotmail.com>
2021-06-30 19:38:49 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, {useState,useRef} from "react";
+import {Canvas, useFrame} from "@react-three/fiber";
+
+import {softShadows, MeshWobbleMaterial, OrbitControls} from "@react-three/drei";
+
+import {useSpring, a} from "@react-spring/three"
+
+softShadows()
+
+const SpinningMesh = ({position, args, color, speed}) =>{
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  const [expand, setExpand] = useState(false);
+
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1]
+  })
+
+  return (
+    <a.mesh onClick={() => setExpand(!expand)}
+    scale = {props.scale}
+    castShadow 
+    position={position} 
+    ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={args} />
+      <MeshWobbleMaterial 
+      attach='material' 
+      color={color} 
+      speed={speed} 
+      factor={0.6} />
+    </a.mesh> 
+  );
+};
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Canvas
+    shadowMap
+    colorManagement 
+    camera={{position: [-5,2,10], fov:60}} >
+      <ambientLight intensity={0.3} />
+      <directionalLight 
+      castShadow
+      intensity={1.5} 
+      position={[0, 10, 0]} 
+      shadow-mapSize-width={1024} 
+      shadow-mapSize-height={1024} 
+      shadow-camera-far={50}
+      shadow-camera-left={-10}
+      shadow-camera-right={10}
+      shadow-camera-top={10}
+      shadow-camera-botton={-10}
+      />
+      <pointLight intensity={0.5} position={[-10,0,-20]}  />
+      <pointLight intensity={1.5} position={[0,-10, 0]}  />
+
+      <group>
+        <mesh 
+        receiveShadow 
+        rotation={[-Math.PI / 2, 0, 0]} 
+        position={[0, -3, 0]}>
+          <planeBufferGeometry attach='geometry' args={[100,100]} />
+          <shadowMaterial attach='material' opacity={0.3} />
+
+        </mesh>
+      </group>
+
+
+      <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" speed={2}/>
+      <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+      <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
+
+      <OrbitControls />
+    </Canvas>
+    </>
   );
 }
 
